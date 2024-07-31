6b636cada299ca0724dc64eccf18c1ffe6c2f908
shrutiparsodkar02 <2023bit507@sggs.ac.in>
2024-07-31 19:37:41 +0530

    adding codes

---
+++ demo.py
@@ -0,0 +1,26 @@
+'''s1=[];
+s2={};
+s3=();
+s5="";
+s6=0.0;
+s7=0;
+s8=None;
+s9=-1;
+if s1:
+	print("s1");
+elif s2:
+	print("s2");
+elif s3:
+	print("s3");
+elif s5:
+	print("s5");
+elif s6:
+	print("s6");
+elif s7:
+	print("s7");
+elif s8:
+	print("s8");
+elif s9:
+	print("printing true loop");
+'''
+
